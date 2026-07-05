0676738b336702e80ebcc4ee79cbccadf451dc8d
Staf IT <admin.it@lmfebui.com>
2026-07-05 22:26:53 +0700

    Update base path to /portfolio/

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/web-ridwan-portfolio/',
+  base: '/portfolio/',
   plugins: [
     react(),
     tailwindcss(),
